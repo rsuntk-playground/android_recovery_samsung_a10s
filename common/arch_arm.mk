bdf1c3a7aff6fdd0877852cfcfdc0935be7e109f
rsuntk <rsuntk@yukiprjkt.my.id>
2025-08-23 11:31:39 +0700

    a10s: Import reworked recovery tree
    Signed-off-by: rsuntk <rsuntk@yukiprjkt.my.id>

---
+++ common/arch_arm.mk
@@ -0,0 +1,7 @@
+# 1st Arch
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a53
